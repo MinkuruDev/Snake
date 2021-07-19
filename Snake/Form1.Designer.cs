75a4d12c8ea89daf727a2f10fd72cff16a9ea235
Mars <52197560+MinkuruDev@users.noreply.github.com>
2021-07-19 15:01:18 +0700

    complete create ground, snake and food

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Lucida Sans Typewriter", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.Location = new System.Drawing.Point(0, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(106, 23);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(761, 512);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        const int length = 40, width = 25, snakeSize = 20;
+        List<Label> labelList = new List<Label>();
+        int headX, headY, tailX, tailY, foodX, foodY;
+        bool[,] visit;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            for(int i = 0; i<length; i++) {
+                for(int j=0; j<width; j++)
+                {
+                    Label label = new Label();
+                    label.Location = new Point(i * snakeSize , j * snakeSize + 50);
+                    label.BackColor = Color.Gray;
+                    label.Size = new Size(snakeSize, snakeSize);
+                    label.Enabled = false;
+
+                    labelList.Add(label);
+                    this.Controls.Add(label);
+                }
+            }
+
+            visit = new bool[length, width];
+            headX = 20;
+            headY = 10;
+            tailX = 20;
+            tailY = 11;
+
+            Label head = getLabelByPositon(headX, headY);
+            visit[headX, headY] = true;
+            head.BackColor = Color.Red;
+
+            Label tail = getLabelByPositon(tailX, tailY);
+            visit[tailX, tailY] = true;
+            tail.BackColor = Color.Red;
+
+            generateFood();
+        }
+
+        void generateFood()
+        {
+            Random random = new Random();
+
+            do
+            {
+                foodX = random.Next(length);
+                foodY = random.Next(width);
+
+            } while (visit[foodX, foodY]);
+
+            Label food = getLabelByPositon(foodX, foodY);
+            food.BackColor = Color.Orange;
+        }
+
+        Label getLabelByPositon(int x, int y)
+        {
+            return labelList[x * width + y];
+        }
+
+        Label getLabelByPositon(Point p)
+        {
+            return getLabelByPositon(p.X, p.Y);
+        }
+    }
+}
