20b06ea61c55c3f93136948a27c4db77063de883
Mars <52197560+MinkuruDev@users.noreply.github.com>
2021-07-24 19:33:28 +0700

    add sfx, map

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -30,11 +30,14 @@ namespace Snake
         private void InitializeComponent()
         {
             this.lblScore = new System.Windows.Forms.Label();
-            this.lblDifficulity = new System.Windows.Forms.Label();
+            this.lblDifficulty = new System.Windows.Forms.Label();
             this.btnIncrease = new System.Windows.Forms.Button();
             this.btnDecrease = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
+            this.cbbMap = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreateMap = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblScore
@@ -47,19 +50,19 @@ namespace Snake
             this.lblScore.TabIndex = 0;
             this.lblScore.Text = "Score: 0";
             // 
-            // lblDifficulity
+            // lblDifficulty
             // 
-            this.lblDifficulity.AutoSize = true;
-            this.lblDifficulity.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDifficulity.Location = new System.Drawing.Point(679, 14);
-            this.lblDifficulity.Name = "lblDifficulity";
-            this.lblDifficulity.Size = new System.Drawing.Size(23, 28);
-            this.lblDifficulity.TabIndex = 1;
-            this.lblDifficulity.Text = "5";
+            this.lblDifficulty.AutoSize = true;
+            this.lblDifficulty.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDifficulty.Location = new System.Drawing.Point(132, 28);
+            this.lblDifficulty.Name = "lblDifficulty";
+            this.lblDifficulty.Size = new System.Drawing.Size(23, 28);
+            this.lblDifficulty.TabIndex = 1;
+            this.lblDifficulty.Text = "5";
             // 
             // btnIncrease
             // 
-            this.btnIncrease.Location = new System.Drawing.Point(708, 12);
+            this.btnIncrease.Location = new System.Drawing.Point(161, 28);
             this.btnIncrease.Name = "btnIncrease";
             this.btnIncrease.Size = new System.Drawing.Size(30, 30);
             this.btnIncrease.TabIndex = 2;
@@ -69,7 +72,7 @@ namespace Snake
             // 
             // btnDecrease
             // 
-            this.btnDecrease.Location = new System.Drawing.Point(643, 12);
+            this.btnDecrease.Location = new System.Drawing.Point(96, 26);
             this.btnDecrease.Name = "btnDecrease";
             this.btnDecrease.Size = new System.Drawing.Size(30, 30);
             this.btnDecrease.TabIndex = 3;
@@ -81,15 +84,15 @@ namespace Snake
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(542, 14);
+            this.label1.Location = new System.Drawing.Point(0, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 28);
+            this.label1.Size = new System.Drawing.Size(90, 28);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Difficulity";
+            this.label1.Text = "Difficulty";
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(333, 17);
+            this.btnStart.Location = new System.Drawing.Point(323, 31);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(94, 29);
             this.btnStart.TabIndex = 5;
@@ -97,17 +100,57 @@ namespace Snake
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // cbbMap
+            // 
+            this.cbbMap.FormattingEnabled = true;
+            this.cbbMap.Items.AddRange(new object[] {
+            "Nothing",
+            "Box",
+            "Divide 4",
+            "Factory",
+            "X"});
+            this.cbbMap.Location = new System.Drawing.Point(614, 12);
+            this.cbbMap.Name = "cbbMap";
+            this.cbbMap.Size = new System.Drawing.Size(135, 28);
+            this.cbbMap.TabIndex = 6;
+            this.cbbMap.Text = "Map";
+            this.cbbMap.SelectedIndexChanged += new System.EventHandler(this.cbbMap_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(556, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Map";
+            // 
+            // btnCreateMap
+            // 
+            this.btnCreateMap.Location = new System.Drawing.Point(607, 43);
+            this.btnCreateMap.Name = "btnCreateMap";
+            this.btnCreateMap.Size = new System.Drawing.Size(142, 27);
+            this.btnCreateMap.TabIndex = 8;
+            this.btnCreateMap.Text = "Create New Map";
+            this.btnCreateMap.UseVisualStyleBackColor = true;
+            this.btnCreateMap.Visible = false;
+            this.btnCreateMap.Click += new System.EventHandler(this.btnCreateMap_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(761, 512);
+            this.Controls.Add(this.btnCreateMap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbMap);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDecrease);
             this.Controls.Add(this.btnIncrease);
-            this.Controls.Add(this.lblDifficulity);
+            this.Controls.Add(this.lblDifficulty);
             this.Controls.Add(this.lblScore);
             this.KeyPreview = true;
             this.Name = "frmMain";
@@ -121,11 +164,14 @@ namespace Snake
         #endregion
 
         private System.Windows.Forms.Label lblScore;
-        private System.Windows.Forms.Label lblDifficulity;
+        private System.Windows.Forms.Label lblDifficulty;
         private System.Windows.Forms.Button btnIncrease;
         private System.Windows.Forms.Button btnDecrease;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cbbMap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreateMap;
     }
 }
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -7,34 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Media;
 
 namespace Snake
 {
     public partial class frmMain : Form
     {
         // global variable need for program
-        const int cols = 40, rows = 25, snakeSize = 20;
-        List<Label> labelList = new List<Label>();
-        Queue<Point> snakeBody = new Queue<Point>();
+        const int cols = 40, rows = 25, snakeSize = 20, marginTop = 70;
+        List<Label> labelList = new List<Label>(); // label list hold label play gruond
+        Queue<Point> snakeBody = new Queue<Point>(); // snakebody
         Point headPoint, tailPoint, foodPoint;
-        bool[,] visit;
-        int score, difficulity;
+        bool[,] visit; // ground
+        bool keyDelay; // prevent some dumb die
+        int score, difficulty;
         Timer timer = new Timer();
+        string path; // path to program file
 
         enum Direction
         {
             up, down, left, right
         }
-        Direction currentDirection;
+        Direction currentDirection; // move direction
 
         public frmMain()
         {
             InitializeComponent();
             this.KeyDown += FrmMain_KeyDown;
+            setPath();
+        }
+
+        private void setPath()
+        {
+            // set program path to map and sfx
+            path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            List<string> l = path.Split("\\").ToList();
+            l.Remove(l[l.Count - 1]);
+            path = "";
+            foreach(string s in l)
+            {
+                path += s + "\\";
+            }
         }
 
         private void FrmMain_KeyDown(object sender, KeyEventArgs e)
         {
+            if (keyDelay) return;
+            if (!timer.Enabled) return;
             switch (e.KeyCode)
             {
                 case Keys.Up:
@@ -58,34 +78,41 @@ namespace Snake
                     currentDirection = Direction.right;
                     break;
             }
+
+            keyDelay = true;
         }
 
         private void startTimer()
         {
-            timer.Interval = 300 - 30 * difficulity;
+            timer.Interval = 250 - 25 * difficulty;
             timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             snakeMove();
+            keyDelay = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             createGround();
+            cbbMap.SelectedIndex = 0; // select default map
             createSnakeBody();
             timer.Tick += Timer_Tick;
+            keyDelay = false;
         }
 
         private void createSnakeBody()
         {
+            snakeBody.Clear();
+
             // add snake tail to queue
-            tailPoint = new Point(cols / 2, rows - 1);
+            tailPoint = new Point(cols / 2, rows - 2);
             snakeBody.Enqueue(tailPoint);
 
             // add snake head to queue
-            headPoint = new Point(cols / 2, rows - 2);
+            headPoint = new Point(cols / 2, rows - 3);
             snakeBody.Enqueue(headPoint);
 
             Label head = getLabelByPositon(headPoint);
@@ -104,43 +131,66 @@ namespace Snake
                 for (int j = 0; j < rows; j++)
                 {
                     Label label = new Label();
-                    label.Location = new Point(i * snakeSize, j * snakeSize + 50);
+                    label.Location = new Point(i * snakeSize, j * snakeSize + marginTop);
                     label.BackColor = Color.Green;
                     label.Size = new Size(snakeSize, snakeSize);
                     label.Enabled = false;
+                    label.MouseDown += Label_MouseDown; // for developer develop map only
 
                     labelList.Add(label);
                     this.Controls.Add(label);
                 }
             }
             
-            difficulity = Convert.ToInt32(lblDifficulity.Text);
+            difficulty = Convert.ToInt32(lblDifficulty.Text);
             visit = new bool[cols, rows];
+        }
+
+        private void Label_MouseDown(object sender, MouseEventArgs e)
+        {
+            // for develop map only
+            Label label = (Label)sender;
+            if(e.Button == MouseButtons.Left)
+            {
+                label.BackColor = Color.Black;
+                int x = label.Location.X / snakeSize;
+                int y = (label.Location.Y - marginTop) / snakeSize;
+                visit[x, y] = true;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                label.BackColor = Color.Green;
+                int x = label.Location.X / snakeSize;
+                int y = (label.Location.Y - marginTop) / snakeSize;
+                visit[x, y] = false;
+            }
         }
 
         private void btnDecrease_Click(object sender, EventArgs e)
         {
-            if (difficulity == 1) return;
-            difficulity--;
-            lblDifficulity.Text = difficulity.ToString();
+            if (difficulty == 1) return;
+            difficulty--;
+            lblDifficulty.Text = difficulty.ToString();
         }
 
         private void btnIncrease_Click(object sender, EventArgs e)
         {
-            if (difficulity == 9) return;
-            difficulity++;
-            lblDifficulity.Text = difficulity.ToString();
+            if (difficulty == 9) return;
+            difficulty++;
+            lblDifficulty.Text = difficulty.ToString();
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            resetGround();
+            createObtancle();
             createSnakeBody();
             generateFood();
             startTimer();
+            // disable something
             btnDecrease.Enabled = false;
             btnIncrease.Enabled = false;
             btnStart.Enabled = false;
+            cbbMap.Enabled = false;
         }
 
         private void resetGround()
@@ -158,31 +208,112 @@ namespace Snake
                 }
             }
 
-            currentDirection = Direction.up;
+            currentDirection = Direction.up; // default direction
             snakeBody.Clear();
             score = 0;
             lblScore.Text = "Score: 0";
-        }  
+        }
+
+        private void cbbMap_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            resetGround();
+            createObtancle();
+            createSnakeBody();
+        }
+
+        void createObtancle(int x, int y)
+        {
+            // create obtancle at location x y
+            Label obtancle = getLabelByPositon(new Point(x, y));
+            visit[x, y] = true;
+            obtancle.BackColor = Color.Black;
+        }
+
+        private void btnCreateMap_Click(object sender, EventArgs e)
+        {
+            // for develop map only
+            Button button = (Button)sender;
+            if(button.Text == "Create New Map")
+            {
+                button.Text = "Done";
+                foreach(Label label in labelList)
+                {
+                    label.Enabled = true;
+                }
+                return;
+            }
+
+            button.Text = "Create New Map";
+            string mapPath = path + "map\\" + "newmap.txt";
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                for (int i = 0; i < cols; i++)
+                {
+                    for (int j = 0; j < rows; j++)
+                    {
+                        if (visit[i, j]) sw.WriteLine(i + " " + j);
+                    }
+                }
+            }
+        }
+
+        void createObtancle()
+        {
+            // create full map on ground
+
+            // get the path to map file
+            string mapPath = path + "map\\"; 
+            mapPath += cbbMap.SelectedItem.ToString() + ".txt";
+
+            int x, y;
+            resetGround();
+
+            if (!File.Exists(mapPath)) return;
+            // read file
+            // an obtancle in file is write x y
+            using (StreamReader sr = File.OpenText(mapPath))
+            {
+                string s;
+                List<string> lists = new List<string>();
+                while((s = sr.ReadLine()) != null)
+                {
+                    lists = s.Split(" ").ToList();
+                    x = Convert.ToInt32(lists[0]);
+                    y = Convert.ToInt32(lists[1]);
+                    createObtancle(x,y);
+                }
+                
+            }
+            
+        }
 
         void snakeMove()
         {
+            // create point have same location with head point
             Point temp = new Point(headPoint.X, headPoint.Y);
+            // new location of temp base on direction
             switch (currentDirection)
             {
                 case Direction.up:
                     temp.Y--;
+                    if (temp.Y < 0) temp.Y = rows - 1;
                     break;
                 case Direction.down:
                     temp.Y++;
+                    if (temp.Y >= rows) temp.Y = 0;
                     break;
                 case Direction.left:
                     temp.X--;
+                    if (temp.X < 0) temp.X = cols - 1;
                     break;
                 case Direction.right:
                     temp.X++;
+                    if (temp.X >= cols) temp.X = 0;
                     break;
             }
             headPoint = temp;
+            // put snake head to queue
             snakeBody.Enqueue(headPoint);
 
             if (checkGameOver(temp))
@@ -197,12 +328,18 @@ namespace Snake
 
             if(headPoint.X == foodPoint.X && headPoint.Y == foodPoint.Y)
             {
-                score += difficulity;
+                // snake eat food
+                string eatPath = path + "sound\\" + "eat.wav";
+                SoundPlayer soundPlayer = new SoundPlayer(eatPath);
+                soundPlayer.Play();
+
+                score += difficulty;
                 lblScore.Text = "Score: " + score;
                 generateFood();
             }
             else
             {
+                // snake not eat anything
                 tailPoint = snakeBody.Dequeue();
                 Label tail = getLabelByPositon(tailPoint);
                 tail.BackColor = Color.Green;
@@ -213,17 +350,19 @@ namespace Snake
         private void gameOver()
         {
             timer.Stop();
+            string hitPath = path + "sound\\" + "hit.wav";
+            SoundPlayer soundPlayer = new SoundPlayer(hitPath);
+            soundPlayer.Play();
             MessageBox.Show("Game over\n" + lblScore.Text);
             btnStart.Enabled = true;
             btnDecrease.Enabled = true;
             btnIncrease.Enabled = true;
+            cbbMap.Enabled = true;
         }
 
         private bool checkGameOver(Point point)
         {
-            int x = point.X;
-            int y = point.Y;
-            return x < 0 || x >= cols || y < 0 || y >= rows || visit[x, y];
+            return visit[point.X, point.Y];
         }
 
         void generateFood()
